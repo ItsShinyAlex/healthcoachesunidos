c32543b2df2bce72205b307fe6efdcaa17287a20
Jesus <jesus@esbrillante.mx>
2018-05-11 12:43:46 -0500

    sitio demo

--- templates/home.php
+++ templates/home.php
@@ -7,11 +7,13 @@
             <img id="logo" src="img/logo.png" alt="center">
         </header>
         
-        <section id="intro"></section>
+        <section id="intro">
+        	<img class="hide-desktop" src="<?php echo get_stylesheet_directory_uri(); ?>/img/responsive/slide.jpg" alt="">
+        </section>
         
         <section id="mision">            
                 <div class="flex">
-                    <div class="columna fondo"></div>
+                    <div class="sin-padding columna fondo"></div>
                     <div class="columna">
                         <h2>Misión</h2>
                         <p>
@@ -44,9 +46,27 @@
         
         <section id="banner">
             <div class="flex">
-                <div class="columna"></div>
-                <div class="columna">
-                    <img src="img/foro.png" alt="">
+                <div class="columna sin-padding">
+                	<img class="hide-desktop" src="<?php echo get_stylesheet_directory_uri(); ?>/img/responsive/logo-evento.jpg" alt="">
+
+                	 <img class="banner-pc hide-movil" src="<?php echo get_stylesheet_directory_uri(); ?>/img/banner.jpg" alt="">
+                </div>
+                <div class="columna">                   
+
+                    <div class="movil">
+                    	<h2>El primer gran foro de Salud Integral en México</h2>
+
+                    	<ul>
+                    		<li>Nutrición holística</li>
+                    		<li>Alimentación consciente - Mindfulness</li>
+                    		<li>Clean eating</li>
+                    		<li>Cambio de hábitos</li>
+                    		<li>Salud emocional</li>
+                    		<li>Fitness</li>
+                    		<li>Talleres de sanación holística: Yoga, meditación</li>
+                    		<li>Talleres de cocina saludable</li>
+                    	</ul>
+                    </div>
                 </div>
             </div>
         </section>
@@ -58,7 +78,42 @@
                 <?php 
                     echo do_shortcode('[caldera_form id="CF5ad15a1cc0472"]');
                  ?>
+           
+
+            
+             </div>
+
+             <div class="full">
+             	<div class="contenedor">
+             		
+             	
+             	<div class="info-contact flex">
+            	<div class="columna">
+            		<a href="https://api.whatsapp.com/send?phone=5553700915">
+            		<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/whatsapp.svg" alt="">
+            		<p>
+            			(55) 53700915
+            		</p>
+            		</a>
+            	</div>
+            	<div class="columna">
+            		<a href="mailto:info@healthcoachesunidos.org">
+            		<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/email.svg" alt="">
+						<p>info@healthcoachesunidos.org</p>
+            		</a>
+            	</div>
+            	<div class="columna">
+            		<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/address.svg" alt="">
+            		<p>
+            			Tlaxcala 127 interior 702 <br>
+						Roma Sur <br>
+						Cuauhtémoc CDMX <br>
+						CP 06760.
+            		</p>
+            	</div>
             </div>
+            </div>
+             </div>
         </section>
 <?php 
     get_footer();
